472fc479521d11924a54edfdb2bb1e378d9d05ed
Joshua Meekhof <jmeekhof@twotheleft.com>
2021-06-18 13:10:01 -0400

    Allow nvim to manage vim alias

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,12 +1,8 @@
-
-alias vim=nvim
-
 set -g theme_date_format "+%Y-%m-%d %H:%M"
 
 set -x GOPATH $HOME/go
 set -x GOBIN $GOPATH/bin
 set -x FZF_DEFAULT_COMMAND "rg --files --hidden --follow --glob '!.git'"
-set -x MANPAGER 'nvim +Man!'
 
 # set -g fish_user_paths "/usr/local/opt/openjdk@11/bin" $fish_user_paths
 status --is-interactive; and pyenv init - | source
