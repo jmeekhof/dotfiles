834c3d349e57c86b289095334a74d511d00bc33d
Joshua Meekhof <jmeekhof@twotheleft.com>
2022-03-15 16:36:02 -0400

    Add fish function changes

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,7 +2,6 @@ set -g theme_date_format "+%Y-%m-%d %H:%M"
 
 set -x GOPATH $HOME/go
 set -x GOBIN $GOPATH/bin
-set -x FZF_DEFAULT_COMMAND "rg --files --hidden --follow --glob '!.git'"
 
 # set -g fish_user_paths "/usr/local/opt/openjdk@11/bin" $fish_user_paths
 status --is-interactive; and pyenv init - | source
@@ -13,6 +12,5 @@ if [ -f "$HOME/google-cloud-sdk/path.fish.inc" ];
 	. "$HOME/google-cloud-sdk/path.fish.inc"; 
 end
 
-if [ -f '/usr/share/fzf/shell/key-bindings.fish' ]; 
-	. '/usr/share/fzf/shell/key-bindings.fish';
-end
+
+
