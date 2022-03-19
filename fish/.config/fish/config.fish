1df82e4fbee812f4d5afa2fe0ee8ffae5fb0d703
Joshua Meekhof <jmeekhof@twotheleft.com>
2022-03-19 10:56:26 -0400

    Remove pyenv commands.
    These should probably be handled in seperate files

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,8 +4,8 @@ set -x GOPATH $HOME/go
 set -x GOBIN $GOPATH/bin
 
 # set -g fish_user_paths "/usr/local/opt/openjdk@11/bin" $fish_user_paths
-status --is-interactive; and pyenv init - | source
-status --is-interactive; and pyenv virtualenv-init - | source
+#status --is-interactive; and pyenv init - | source
+#status --is-interactive; and pyenv virtualenv-init - | source
 
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f "$HOME/google-cloud-sdk/path.fish.inc" ];
