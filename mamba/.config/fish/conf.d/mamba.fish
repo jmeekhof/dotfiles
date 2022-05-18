d45bee207ee145b9e340ff35b0c4c2e17cb685e4
Joshua Meekhof <josh@katanagraph.com>
2022-05-18 12:34:05 -0400

    Add mamba init

---
+++ mamba/.config/fish/conf.d/mamba.fish
@@ -0,0 +1,7 @@
+# >>> mamba initialize >>>
+# !! Contents within this block are managed by 'mamba init' !!
+set -gx MAMBA_EXE "/home/jmeekhof/.local/bin/micromamba"
+set -gx MAMBA_ROOT_PREFIX "/home/jmeekhof/micromamba"
+eval "/home/jmeekhof/.local/bin/micromamba" shell hook --shell fish --prefix "/home/jmeekhof/micromamba" | source
+# <<< mamba initialize <<<
+
