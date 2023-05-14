cdff0e6b2bf817f6e11dde9ca355da3d342fbc56
Joshua Meekhof <jmeekhof@twotheleft.com>
2023-05-14 17:43:27 -0400

    Remove old tmux config

--- tmux/.config/fish/conf.d/tmux.fish
+++
@@ -1,8 +0,0 @@
-# Credit for this gem: https://www.markhansen.co.nz/auto-start-tmux/
-if type -q tmux
-	if status is-interactive 
-		and not set -q TMUX
-		tmux-start.sh #tmux new-session -A -s main
-	end
-end
-
