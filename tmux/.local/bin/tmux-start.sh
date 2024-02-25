3a9613b7aaf02c7622c5fd348af9a57d842e290b
Josh Meekhof <josh.meekhof@charliehealth.com>
2024-02-25 09:39:48 -0500

    Update general configs

--- tmux/.local/bin/tmux-start.sh
+++ tmux/.local/bin/tmux-start.sh
@@ -7,13 +7,13 @@ session=main
 if ! tmux has -t $session; then
 	tmux new-session -d -s $session
 
-	window=1
-	tmux rename-window -t $session:$window 'MPD'
-	tmux send-keys -t $session:$window 'ncmpcpp' C-m
+	# window=1
+	# tmux rename-window -t $session:$window 'MPD'
+	# tmux send-keys -t $session:$window 'ncmpcpp' C-m
 
 	window=2
-	tmux new-window -t $session:$window -c ~/katana/katana-tools -n 'k tool'
-	tmux send-keys -t $session:$window 'git fetch --all' C-m
+	tmux new-window -t $session:$window -c ~/projects/ch -n 'ch'
+	# tmux send-keys -t $session:$window 'git fetch --all' C-m
 	tmux split-window -t $session:$window -c "#{pane_current_path}"
 	tmux send-keys -t $session:$window 'nvim' C-m
 fi
