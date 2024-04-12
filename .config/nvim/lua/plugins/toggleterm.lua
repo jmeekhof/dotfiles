809baf19a720de69350dbd25889c6a8801454dcf
Josh Meekhof <jmeekhof@twotheleft.com>
2024-04-12 08:26:25 -0400

    Add toggleterm

---
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,68 @@
+return {
+  {
+    "akinsho/toggleterm.nvim",
+    config = true,
+    cmd = "ToggleTerm",
+    keys = {
+      {
+        "<leader>Tf",
+        function()
+          local count = vim.v.count1
+          require("toggleterm").toggle(count, 0, LazyVim.root.get(), "float")
+        end,
+        desc = "ToggleTerm (float root dir)",
+      },
+      {
+        "<leader>Th",
+        function()
+          local count = vim.v.count1
+          require("toggleterm").toggle(count, 15, LazyVim.root.get(), "horizontal")
+        end,
+        desc = "ToggleTerm (horizontal root dir)",
+      },
+      {
+        "<leader>Tv",
+        function()
+          local count = vim.v.count1
+          require("toggleterm").toggle(count, vim.o.columns * 0.4, LazyVim.root.get(), "vertical")
+        end,
+        desc = "ToggleTerm (vertical root dir)",
+      },
+      {
+        "<leader>Tn",
+        "<cmd>ToggleTermSetName<cr>",
+        desc = "Set term name",
+      },
+      {
+        "<leader>Ts",
+        "<cmd>TermSelect<cr>",
+        desc = "Select term",
+      },
+      {
+        "<leader>Tt",
+        function()
+          require("toggleterm").toggle(1, 100, LazyVim.root.get(), "tab")
+        end,
+        desc = "ToggleTerm (tab root_dir)",
+      },
+      {
+        "<leader>TT",
+        function()
+          require("toggleterm").toggle(1, 100, vim.loop.cwd(), "tab")
+        end,
+        desc = "ToggleTerm (tab cwd_dir)",
+      },
+    },
+    opts = {
+      open_mapping = [[<C-t>]],
+      direction = "float",
+      shade_filetypes = {},
+      hide_numbers = true,
+      insert_mappings = true,
+      terminal_mappings = true,
+      start_in_insert = true,
+      close_on_exit = true,
+      shell = "fish --login",
+    },
+  },
+}
