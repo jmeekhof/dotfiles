25b53fe59c06f7ff3ee8c88a4fc1ae4424f07bf8
Josh Meekhof <jmeekhof@twotheleft.com>
2024-06-13 07:09:52 -0400

    Update configs to current version

--- .config/nvim/lua/plugins/dashboard.lua
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -104,7 +104,7 @@ return {
         header = vim.split(logo, "\n"),
       -- stylua: ignore
       center = {
-        { action = LazyVim.telescope("files"),                                    desc = " Find File",       icon = " ", key = "f" },
+        { action = LazyVim.pick("files"),                                    desc = " Find File",       icon = " ", key = "f" },
         { action = "ene | startinsert",                                        desc = " New File",        icon = " ", key = "n" },
         { action = "Telescope oldfiles",                                       desc = " Recent Files",    icon = " ", key = "r" },
         { action = "Telescope live_grep",                                      desc = " Find Text",       icon = " ", key = "g" },
