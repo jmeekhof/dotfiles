98b3d709d781ffac05495b1ac7859686af0bb3e4
Josh Meekhof <jmeekhof@twotheleft.com>
2025-02-25 10:49:50 -0500

    Update colorscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,58 +1,17 @@
 return {
   {
-    {
-      "catppuccin/nvim",
-      name = "catppuccin",
-      priority = 1000,
-      opts = {
-        flavor = "latte",
-        transparent_background = true,
-      },
-    },
-    { "craftzdog/solarized-osaka.nvim" },
-    {
-      "LazyVim/LazyVim",
-      opts = {
-        colorscheme = "catppuccin",
-      },
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "frappe",
+      transparent_background = true,
     },
   },
-  --[[
-  "folke/tokyonight.nvim",
-  lazy = true,
-  opts = {
-    style = "storm",
-    transparent = true,
-    styles = {
-      sidebars = "transparent",
-      -- floats = "transparent",
-    },
-  },
-  -- ]]
-
-  --[[
-  -- add gruvbox
-  --{ "ellisonleao/gruvbox.nvim" },
-
-  -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      -- colorscheme = "gruvbox",
+      colorscheme = "catppuccin",
     },
   },
-  --]]
-
-  --[[
-return {
-  { "maxmx03/solarized.nvim" },
-
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      -- colorscheme = "solarized",
-    },
-  },
-}
-  --]]
 }
