f4dcf331e5633dc934a6c72cef408a43418bb7ac
Josh Meekhof <josh.meekhof@charliehealth.com>
2025-08-22 21:42:42 -0400

    Colorscheming

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,17 +1,7 @@
 return {
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    opts = {
-      flavour = "frappe",
-      transparent_background = true,
-    },
-  },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
-    },
-  },
+  -- { "catppuccin/nvim", name = "catppuccin", opts = { transparent_background = true } },
+  --
+  -- { "LazyVim/LazyVim", opts = { colorscheme = "catppuccin" } },
+  { "folke/tokyonight.nvim", name = "tokyonight", opts = { style = "storm" } },
+  { "LazyVim/LazyVim", opts = { colorscheme = "tokyonight" } },
 }
