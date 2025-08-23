66bdb8948a8640e26c2788d12a317eaaf4666a10
Josh Meekhof <josh.meekhof@charliehealth.com>
2025-08-22 21:43:07 -0400

    Update plugins

--- .config/nvim/lua/plugins/nvim_lsp_go_settings.lua
+++ .config/nvim/lua/plugins/nvim_lsp_go_settings.lua
@@ -2,12 +2,13 @@ local function file_exists(name)
   local f = io.open(name, "r")
   return f ~= nil and io.close(f)
 end
-return {
-  "neovim/nvim-lspconfig",
-  opts = function(_, opts)
-    local drv = "./tools/gopackagesdriver.sh"
-    if file_exists(drv) then
-      vim.tbl_deep_extend("keep", opts.servers.gopls.settings.gopls, { env = { GOPACKAGESDRIVER = drv } })
-    end
-  end,
-}
+return {}
+-- return {
+--   "neovim/nvim-lspconfig",
+--   opts = function(_, opts)
+--     local drv = "./tools/gopackagesdriver.sh"
+--     if file_exists(drv) then
+--       vim.tbl_deep_extend("keep", opts.servers.gopls.settings.gopls, { env = { GOPACKAGESDRIVER = drv } })
+--     end
+--   end,
+-- }
