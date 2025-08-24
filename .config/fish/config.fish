76af4789fd3c2f16aa2dd8926f34833f45564da5
Josh Meekhof <josh.meekhof@charliehealth.com>
2025-08-24 12:24:58 -0400

    fix

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,8 +3,7 @@ set -g theme_date_format "+%Y-%m-%d %H:%M"
 set -x GOPATH $HOME/go
 set -x GOBIN $GOPATH/bin
 
-
 set -x GPG_TTY (tty)
 
 fish_vi_key_bindings
-~/.local/bin/mise activate fish | source
+mise activate fish | source
